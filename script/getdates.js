a0fcb045a2f965e1899ac554a31b33e2b582e93c
mtafunai <roy.tafunai10@gmail.com>
2025-05-10 21:06:33 +1200

    js

--- script/getdates.js
+++ script/getdates.js
@@ -1,6 +1,15 @@
-const today = new Date();
+const full = document.querySelector("#full");
+const worldfull = document.querySelector("#world-full");
+const short = document.querySelector("#short");
+const medium = document.querySelector("#medium");
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const dayofweek = document.querySelector("#dayofweek");
 
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+const date = new Date();
+
+ffull.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
 	"en-US",
 	{
 		dateStyle: "full"
@@ -29,3 +38,4 @@ year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}<
 month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
 day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
 dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+
