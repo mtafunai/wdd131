0a8207627277f4630e40b2a00a36deef532433eb
mtafunai <roy.tafunai10@gmail.com>
2025-05-10 19:43:09 +1200

    dates

--- scipts/getdates.js
+++ scipts/getdates.js
@@ -1,4 +1,31 @@
-const currentYear = document.getElementById("currentyear");
-currentYear.innerText = new Date().getFullYear();
+const today = new Date();
 
-document.querySelector("#lastModified").innerHTML = `Last modified: ${document.lastModified}`;
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-UK",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
+medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "medium"
+	}
+).format(today)}</span>`;
+
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
+day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
+dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
