c725ac56c4fe7f32f36fa40cdff9c2edadeac9f4
mtafunai <roy.tafunai10@gmail.com>
2026-09-12 22:30:26 +1200

    week 2

---
+++ scripts/temples.js
@@ -0,0 +1,30 @@
+javascript
+
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+
+
+document.querySelector("#lastModified").textContent =
+    `Last Modification: ${document.lastModified}`;
+
+
+
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("#navigation");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    const isOpen = navigation.classList.contains("open");
+
+    menuButton.textContent = isOpen ? "✕" : "☰";
+
+    menuButton.setAttribute("aria-expanded", isOpen);
+
+    menuButton.setAttribute(
+        "aria-label",
+        isOpen ? "Close navigation menu" : "Open navigation menu"
+    );
+});
